0607c0ec2c11754c8f7c391bd49dcfcc1d498507
Noor Al Darraji <077068@yahoo.com>
2019-08-14 17:29:27 -0500

    Wrap App with provider from RR, and created the MTP function :white_check_mark:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { carReducer } from './reducers/index'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+export const store = createStore(carReducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}> <App /> <Provider/> , rootElement);
